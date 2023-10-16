997090f777a1f7385b0addbfc06ac8d90393ec61
MethasMP <b6332877@gmail.com>
2023-10-16 11:39:13 +0700

    fifth-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsService } from './components/product-details/serivce/products.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IndexComponent } from './components/index/index.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ClientComponent } from './components/client/client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductDetailsComponent,
+    IndexComponent,
+    AdminComponent,
+    ClientComponent,
     
 
   ],
